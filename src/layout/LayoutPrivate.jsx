abef50d26e4e13f877f20dfb7966a1dae169c21c
Ester Termens <estertermens@hotmail.com>
2023-09-11 10:43:45 +0200

    navigation

--- src/layout/LayoutPrivate.jsx
+++ src/layout/LayoutPrivate.jsx
@@ -1,20 +1,22 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate, Navigate } from "react-router-dom";
 import { useUserContext } from "../context/UserContext";
 import { useEffect } from "react";
 
 const LayoutPrivate = () => {
   const { user } = useUserContext(); // traigo el user
-  const navigate = useNavigate();
+
+  // const navigate = useNavigate();
+
   //con el useEffect, compruebo que cada vez qeu se cambie el user, haga algo. En este caso volver a la home
-  useEffect(() => {
+  //Existe una alternativa al useEffect, que es utilizar en Navigate de react-router-dom directamente
+
+  /*useEffect(() => {
     console.log(user);
     if (!user) {
       navigate("/");
     }
-  }, [user]);
+  }, [user]);*/
 
-  return (
-    <Outlet /> //aqui solo ponemos el outlet, porque comparte el navbar y foolter del otro layout
-  );
+  return <>{user ? <Outlet /> : <Navigate to="/" />}</>;
 };
 export default LayoutPrivate;
